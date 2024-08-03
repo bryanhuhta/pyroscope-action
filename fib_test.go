15dd5805938793bb952dd7772c8caab77ba8ff1a
Bryan Huhta <bryan.huhta@grafana.com>
2024-08-02 19:22:03 -0700

    Add another benchmark

--- fib_test.go
+++ fib_test.go
@@ -12,6 +12,14 @@ func BenchmarkFib10(b *testing.B) {
 	result = n
 }
 
+func BenchmarkFib100(b *testing.B) {
+	n := 0
+	for i := 0; i < b.N; i++ {
+		n = Fib(100)
+	}
+	result = n
+}
+
 func BenchmarkFib100_000_000(b *testing.B) {
 	n := 0
 	for i := 0; i < b.N; i++ {
